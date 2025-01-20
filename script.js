ed1c7140f0c89eb437b8d4f8ebb07be632d734d5
OliOSZ <146973447+OliOSZ@users.noreply.github.com>
2025-01-20 16:31:46 +0100

    Feedback form
    la til alt

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-function submit(event){
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
+const submit = document.getElementById('submit')
+const feedbackListUi = document.getElementById('userFeedbackList')
+let feedbackList = readFeedbackList()
 
+// if nothing exists why need empty array
+function readFeedbackList(){
+    const feedbackList = JSON.parse(localStorage.getItem("userFeedbackList"));
+    if (feedbackList) {
+        return feedbackList;
+    }
+    return [];
 }
+
+// so i dont have to fix stuff twice
+function renderListElement(userFeedbackItem){
+    const listItem = document.createElement('li');
+    const contentElementUsername= document.createElement('div');
+    contentElementUsername.textContent = userFeedbackItem.username;
+    const contentElementMessage= document.createElement('div');
+    contentElementMessage.textContent = userFeedbackItem.message;
+    listItem.appendChild(contentElementUsername);
+    listItem.appendChild(contentElementMessage);
+    feedbackListUi.appendChild(listItem);
+}
+
+// only gets called once, when you first enter
+function renderFeedbackList(){
+    feedbackList.forEach((item, index) => {
+        renderListElement(item);
+    })
+}
+
+function addFeedback(){
+    const username = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const messageInput = document.getElementById('message').value;
+    const feedbackItem = {username: username, email: email, message: messageInput};
+    
+    feedbackList?.push(feedbackItem);
+    localStorage.setItem("userFeedbackList", JSON.stringify(feedbackList));
+    renderListElement(feedbackItem);
+}
+
+renderFeedbackList();
